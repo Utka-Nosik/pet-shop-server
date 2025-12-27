3bf2f9ecd212c8bd054fd0b3e8aef996846818dd
Bako <bakoabdi77@gmail.com>
2025-12-28 03:16:32 +0500

    Initial commit: Weather App with Gumball theme

--- first_serv.js
+++ first_serv.js
@@ -3,11 +3,16 @@ const express = require('express');
 const fs = require('fs');
 let app = express();
 app.use(express.json());
+const axios = require('axios'); 
 
 app.use(express.static('assets')); 
 
 app.get('/index.html', (request, response) => {
     response.sendFile(__dirname + '/index.html');
+})
+
+app.get('/weather.html', (request, response) => {
+    response.sendFile(__dirname + '/weather.html');
 })
 
 app.get('/', (request, response) => {
@@ -47,7 +52,7 @@ app.get('/hello', (request, response) => {
 app.get('/time', (request, response) => {
     const now = new Date();
     
-    response.send("Present time: " + now.toLocaleTimeString());
+    response.send("Present time: " + now.toDateString());
 });
 
 app.get('/status', (request, response) => {
@@ -130,11 +135,69 @@ app.delete('/items/:id', (req, res) => {
         db.items = newItems; 
         fs.writeFile('data.json', JSON.stringify(db, null, 2), (err) => {
             if (err) return res.status(500).send("Error writing file");
-            res.json(item); 
+            res.json({ message: "Deleted" })
         });
     });
 });
 
+
+app.get('/weather', async (req, res) => {
+    const city = req.query.city; 
+    const apiKey = '5f764c98cec648822301320198aae8ef'; 
+
+    if (!city) {
+        return res.status(400).json({ error: "City name is required" });
+    }
+
+    try {
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        const weatherResponse = await axios.get(weatherUrl);
+        const data = weatherResponse.data;
+
+        const weatherResult = {
+            temperature: data.main.temp,
+            description: data.weather[0].description,
+            icon: data.weather[0].icon, 
+            coordinates: data.coord,
+            feels_like: data.main.feels_like,
+            wind_speed: data.wind.speed,
+            country_code: data.sys.country,
+            rain_volume: (data.rain && data.rain['3h']) ? data.rain['3h'] : 0 
+        };
+
+        let countryInfo = {};
+        try {
+            const countryUrl = `https://restcountries.com/v3.1/alpha/${weatherResult.country_code}`;
+            const countryResponse = await axios.get(countryUrl);
+            const countryData = countryResponse.data[0];
+
+            const currencyKey = Object.keys(countryData.currencies)[0];
+            const languages = Object.values(countryData.languages).join(', '); 
+
+            countryInfo = {
+                currency: countryData.currencies[currencyKey].name,
+                currency_symbol: countryData.currencies[currencyKey].symbol,
+                population: countryData.population,
+                flag: countryData.flags.png,
+                capital: countryData.capital ? countryData.capital[0] : "No Capital", 
+                region: countryData.region, 
+                googleMaps: countryData.maps.googleMaps 
+            };
+        } catch (err) {
+            console.log("Error fetching country data:", err.message);
+            countryInfo = { error: "Could not fetch extra country data" };
+        }
+
+        res.json({
+            weather: weatherResult,
+            country_facts: countryInfo
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error fetching data. Check city name." });
+    }
+});
 
 app.listen(3000, () => {
     console.log("Server started at http://localhost:3000");
